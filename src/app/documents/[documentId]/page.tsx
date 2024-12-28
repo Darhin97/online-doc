6f22d028332ebe4ffefdf18f8c14ab2fbece69e7
Derrick Ankrah <darhin97@gmail.com>
2024-12-28 05:41:00 +0000

    initial commit

---
+++ src/app/documents/[documentId]/page.tsx
@@ -0,0 +1,16 @@
+import { Editor } from "@/app/documents/[documentId]/editor";
+
+interface DocumentIdPageProps {
+  params: Promise<{ documentId: string }>;
+}
+const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
+  const { documentId } = await params;
+
+  return (
+    <div className={"min-h-screen bg-[#fafbfd]"}>
+      <Editor />
+    </div>
+  );
+};
+
+export default DocumentIdPage;
