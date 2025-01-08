3b6927e0fc26e08ec46d1d123b447aad9a401889
Derrick Ankrah <darhin97@gmail.com>
2025-01-08 14:32:02 +0000

    added added nav

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -1,14 +1,40 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
-import DocumentInput from "@/components/document-input";
+import {
+  BoldIcon,
+  FileIcon,
+  FileJsonIcon,
+  FilePenIcon,
+  FilePlusIcon,
+  FileTextIcon,
+  GlobeIcon,
+  ItalicIcon,
+  PrinterIcon,
+  Redo2Icon,
+  Strikethrough,
+  StrikethroughIcon,
+  TextIcon,
+  TrashIcon,
+  UnderlineIcon,
+  Undo2Icon,
+} from "lucide-react";
+import { BsFilePdf } from "react-icons/bs";
+
 import {
   Menubar,
   MenubarContent,
   MenubarItem,
   MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarSub,
+  MenubarSubContent,
+  MenubarSubTrigger,
   MenubarTrigger,
 } from "@/components/ui/menubar";
-import { FileIcon } from "lucide-react";
+import DocumentInput from "@/components/document-input";
 
 const Navbar = () => {
   return (
@@ -25,9 +51,43 @@ const Navbar = () => {
             >
               <MenubarMenu>
                 <MenubarTrigger>File</MenubarTrigger>
-                <MenubarContent>
+                <MenubarContent className={"print:hidden"}>
+                  <MenubarSub>
+                    <MenubarSubTrigger>
+                      <FileIcon className={"size-4 mr-2"} /> Save
+                    </MenubarSubTrigger>
+                    <MenubarSubContent>
+                      <MenubarItem>
+                        <FileJsonIcon className={"size-4 mr-2"} /> JSON
+                      </MenubarItem>
+                      <MenubarItem>
+                        <GlobeIcon className={"size-4 mr-2"} /> HTML
+                      </MenubarItem>
+                      <MenubarItem>
+                        <BsFilePdf className={"size-4 mr-2"} /> PDF
+                      </MenubarItem>
+                      <MenubarItem>
+                        <FileTextIcon className={"size-4 mr-2"} /> Text
+                      </MenubarItem>
+                    </MenubarSubContent>
+                  </MenubarSub>
                   <MenubarItem>
-                    <FileIcon className={"size-4 mr-2"} /> Save
+                    <FilePlusIcon className={"size-4 mr-2"} />
+                    New Document
+                  </MenubarItem>
+                  <MenubarSeparator />
+                  <MenubarItem>
+                    <FilePenIcon className={"size-4 mr-2"} />
+                    Rename
+                  </MenubarItem>
+                  <MenubarItem>
+                    <TrashIcon className={"size-4 mr-2"} />
+                    Remove
+                  </MenubarItem>
+                  <MenubarSeparator />
+                  <MenubarItem onClick={() => window.print()}>
+                    <PrinterIcon className={"size-4 mr-2"} />
+                    Print <MenubarShortcut>&#8984;P</MenubarShortcut>
                   </MenubarItem>
                 </MenubarContent>
               </MenubarMenu>
@@ -39,6 +99,16 @@ const Navbar = () => {
                 >
                   Edit
                 </MenubarTrigger>
+                <MenubarContent>
+                  <MenubarItem>
+                    <Undo2Icon className={"size-4 mr-2"} /> Undo{" "}
+                    <MenubarShortcut>&#8984;Z</MenubarShortcut>
+                  </MenubarItem>
+                  <MenubarItem>
+                    <Redo2Icon className={"size-4 mr-2"} /> Redo{" "}
+                    <MenubarShortcut>&#8984;Y</MenubarShortcut>
+                  </MenubarItem>
+                </MenubarContent>
               </MenubarMenu>
               <MenubarMenu>
                 <MenubarTrigger
@@ -48,6 +118,17 @@ const Navbar = () => {
                 >
                   Insert
                 </MenubarTrigger>
+                <MenubarContent>
+                  <MenubarSub>
+                    <MenubarSubTrigger>Table</MenubarSubTrigger>
+                    <MenubarSubContent>
+                      <MenubarItem>1 x 1</MenubarItem>
+                      <MenubarItem>2 x 2</MenubarItem>
+                      <MenubarItem>3 x 3</MenubarItem>
+                      <MenubarItem>4 x 4</MenubarItem>
+                    </MenubarSubContent>
+                  </MenubarSub>
+                </MenubarContent>
               </MenubarMenu>
               <MenubarMenu>
                 <MenubarTrigger
@@ -57,6 +138,31 @@ const Navbar = () => {
                 >
                   Format
                 </MenubarTrigger>
+                <MenubarContent>
+                  <MenubarSub>
+                    <MenubarSubTrigger>
+                      <TextIcon className={"size-4 mr-2"} /> Text
+                    </MenubarSubTrigger>
+                    <MenubarSubContent>
+                      <MenubarItem>
+                        <BoldIcon className={"size-4 mr-2"} />
+                        Bold
+                      </MenubarItem>
+                      <MenubarItem>
+                        <ItalicIcon className={"size-4 mr-2"} />
+                        Italic
+                      </MenubarItem>
+                      <MenubarItem>
+                        <UnderlineIcon className={"size-4 mr-2"} />
+                        Underline
+                      </MenubarItem>
+                      <MenubarItem>
+                        <StrikethroughIcon className={"size-4 mr-2"} />
+                        Strikethrough
+                      </MenubarItem>
+                    </MenubarSubContent>
+                  </MenubarSub>
+                </MenubarContent>
               </MenubarMenu>
             </Menubar>
           </div>
