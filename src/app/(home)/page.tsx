2bdcfd39a95c004214373910798b0d9a1f19a02e
Derrick Ankrah <darhin97@gmail.com>
2025-01-13 14:10:52 +0000

    added clerk auth and documents template designs

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,17 +1,26 @@
+"use client";
 import { Navbar } from "@/components/navbar";
-import Link from "next/link";
+import { TemplatesGallery } from "@/app/(home)/templates-gallery";
+
+import { useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
 
 const Home = () => {
+  const documents = useQuery(api.documents.get);
+
+  if (documents === undefined) {
+    return <p>loading</p>;
+  }
   return (
     <div className={"min-h-screen flex flex-col"}>
       <div className={"fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4"}>
         <Navbar />
       </div>
       <div className={"mt-16"}>
-        click{" "}
-        <Link href={"/documents/123"}>
-          <span className={"text-blue-500"}>here</span>
-        </Link>
+        <TemplatesGallery />
+        {documents?.map((document) => (
+          <span key={document._id}>{document.title}</span>
+        ))}
       </div>
     </div>
   );
